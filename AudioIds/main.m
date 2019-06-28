e5833c78fabe21ceaef51c9c8889abbe13bf28f1
Jim Bennett <jim.bennett@microsoft.com>
2019-06-28 17:10:48 +0100

    Initial commit

---
+++ AudioIds/main.m
@@ -0,0 +1,146 @@
+//
+//  main.m
+//  AudioIds
+//
+//  Created by Jim Bennett on 28/06/2019.
+//  Copyright © 2019 Jim Bennett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreAudio/CoreAudio.h>
+
+CFArrayRef CreateInputDeviceArray()
+{
+    AudioObjectPropertyAddress propertyAddress = {
+        kAudioHardwarePropertyDevices,
+        kAudioObjectPropertyScopeGlobal,
+        kAudioObjectPropertyElementMaster
+    };
+    
+    UInt32 dataSize = 0;
+    OSStatus status = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &dataSize);
+    if (kAudioHardwareNoError != status) {
+        fprintf(stderr, "AudioObjectGetPropertyDataSize (kAudioHardwarePropertyDevices) failed: %i\n", status);
+        return NULL;
+    }
+    
+    UInt32 deviceCount = (uint32)(dataSize / sizeof(AudioDeviceID));
+    
+    AudioDeviceID *audioDevices = (AudioDeviceID *)(malloc(dataSize));
+    if (NULL == audioDevices) {
+        fputs("Unable to allocate memory", stderr);
+        return NULL;
+    }
+    
+    status = AudioObjectGetPropertyData(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &dataSize, audioDevices);
+    if (kAudioHardwareNoError != status) {
+        fprintf(stderr, "AudioObjectGetPropertyData (kAudioHardwarePropertyDevices) failed: %i\n", status);
+        free(audioDevices);
+        audioDevices = NULL;
+        return NULL;
+    }
+    
+    CFMutableArrayRef inputDeviceArray = CFArrayCreateMutable(kCFAllocatorDefault, deviceCount, &kCFTypeArrayCallBacks);
+    if (NULL == inputDeviceArray) {
+        fputs("CFArrayCreateMutable failed", stderr);
+        free(audioDevices);
+        audioDevices = NULL;
+        return NULL;
+    }
+    
+    // Iterate through all the devices and determine which are input-capable
+    propertyAddress.mScope = kAudioDevicePropertyScopeInput;
+    for (UInt32 i = 0; i < deviceCount; ++i) {
+        // Query device UID
+        CFStringRef deviceUID = NULL;
+        dataSize = sizeof(deviceUID);
+        propertyAddress.mSelector = kAudioDevicePropertyDeviceUID;
+        status = AudioObjectGetPropertyData(audioDevices[i], &propertyAddress, 0, NULL, &dataSize, &deviceUID);
+        if (kAudioHardwareNoError != status) {
+            fprintf(stderr, "AudioObjectGetPropertyData (kAudioDevicePropertyDeviceUID) failed: %i\n", status);
+            continue;
+        }
+        
+        // Query device name
+        CFStringRef deviceName = NULL;
+        dataSize = sizeof(deviceName);
+        propertyAddress.mSelector = kAudioDevicePropertyDeviceNameCFString;
+        status = AudioObjectGetPropertyData(audioDevices[i], &propertyAddress, 0, NULL, &dataSize, &deviceName);
+        if (kAudioHardwareNoError != status) {
+            fprintf(stderr, "AudioObjectGetPropertyData (kAudioDevicePropertyDeviceNameCFString) failed: %i\n", status);
+            continue;
+        }
+        
+        // Determine if the device is an input device (it is an input device if it has input channels)
+        dataSize = 0;
+        propertyAddress.mSelector = kAudioDevicePropertyStreamConfiguration;
+        status = AudioObjectGetPropertyDataSize(audioDevices[i], &propertyAddress, 0, NULL, &dataSize);
+        if (kAudioHardwareNoError != status) {
+            fprintf(stderr, "AudioObjectGetPropertyDataSize (kAudioDevicePropertyStreamConfiguration) failed: %i\n", status);
+            continue;
+        }
+        
+        AudioBufferList *bufferList = (AudioBufferList *)(malloc(dataSize));
+        if (NULL == bufferList) {
+            fputs("Unable to allocate memory", stderr);
+            break;
+        }
+        
+        status = AudioObjectGetPropertyData(audioDevices[i], &propertyAddress, 0, NULL, &dataSize, bufferList);
+        if (kAudioHardwareNoError != status || 0 == bufferList->mNumberBuffers) {
+            if (kAudioHardwareNoError != status)
+                fprintf(stderr, "AudioObjectGetPropertyData (kAudioDevicePropertyStreamConfiguration) failed: %i\n", status);
+            free(bufferList);
+            bufferList = NULL;
+            continue;
+        }
+        
+        free(bufferList);
+        bufferList = NULL;
+        
+        // Add a dictionary for this device to the array of input devices
+        CFStringRef keys    []  = { CFSTR("deviceUID"),     CFSTR("deviceName")};
+        CFStringRef values  []  = { deviceUID,              deviceName};
+        
+        CFDictionaryRef deviceDictionary = CFDictionaryCreate(kCFAllocatorDefault,
+                                                              (const void **)(keys),
+                                                              (const void **)(values),
+                                                              2,
+                                                              &kCFTypeDictionaryKeyCallBacks,
+                                                              &kCFTypeDictionaryValueCallBacks);
+        
+        CFArrayAppendValue(inputDeviceArray, deviceDictionary);
+        
+        CFRelease(deviceDictionary);
+        deviceDictionary = NULL;
+    }
+    
+    free(audioDevices);
+    audioDevices = NULL;
+    
+    // Return a non-mutable copy of the array
+    CFArrayRef immutableInputDeviceArray = CFArrayCreateCopy(kCFAllocatorDefault, inputDeviceArray);
+    CFRelease(inputDeviceArray);
+    inputDeviceArray = NULL;
+    
+    return immutableInputDeviceArray;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        CFArrayRef ids = CreateInputDeviceArray();
+        
+        CFIndex nameCount = CFArrayGetCount( ids );
+        
+        //Iterate through the CFArrayRef and fill the vector
+        for( int i = 0; i < nameCount ; ++i  )
+        {
+            CFStringRef sName = (CFStringRef)CFArrayGetValueAtIndex( ids, i );
+            NSString *aNSString = (__bridge NSString *)sName;
+            
+            NSLog(@"%@", aNSString);
+        }
+    }
+    return 0;
+}
